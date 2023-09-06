43037795e50932f8e2bb3249063e659a2b4d99b5
Евгения <Zolotayledy@yandex.ru>
2023-09-06 11:54:43 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int valueTicket = 13676;
+        int rublesForMiles = 20;
+        int bonusMiles =  valueTicket / rublesForMiles ;
+
+
+        System.out.println("Количество бонусных миль =" + bonusMiles);
+    }
+}
+
